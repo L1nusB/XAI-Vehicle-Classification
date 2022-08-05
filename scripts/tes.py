d7d2bb0dd39d2747cf6aa02b016c19cdcab92ca3
Linus <geckobaum@gmail.com>
2022-08-05 11:56:26 +0200

    Wrapping works with funcs. Need to adapt Registry

--- scripts/tes.py
+++ scripts/tes.py
@@ -1,7 +1,7 @@
 from mmcv.utils import Registry
 from mmseg.datasets.builder import DATASETS
 from mmcls.datasets.builder import DATASETS as D2
-from mmcls.datasets.compcars import CompCars as P
+from mmcls.datasets.compcars import CompCars
 from mmseg.datasets.builder import build_dataset, build_dataloader
 from mmcls.apis.test import single_gpu_test
 from mmcls.models.builder import build_classifier
@@ -11,12 +11,18 @@ from mmcv.runner import load_checkpoint
 
 #DATASETS = Registry('dataset')
 
-@DATASETS.register_module()
-class CompCars(P):
-    def test():
-        print("T")
+# @DATASETS.register_module()
+# class CompCars(P):
+#     def test():
+#         print("T")
+
+@DATASETS.register_module('CompCars')
+def CompCarsWrapper(**kwargs):
+    return CompCars(**kwargs)
 
 def main():
+    print(DATASETS)
+    print("#"*30)
     cfg = mmcv.Config.fromfile("../CAMModels/resnet/compCars_Original/resnet50_b128x2_compcars-original-split.py")
     print(cfg.data.test)
     cfg.data.test['data_prefix'] = "../data/CompCars_sv_original_split/val" 
@@ -33,13 +39,13 @@ def main():
     print(data_loader)
     print(len(dataset))
     model = build_classifier(cfg.model)
-    print(model)
+    #print(model)
     print("#"*30)
     checkpoint = load_checkpoint(model, "../CAMModels/resnet/compCars_Original/latest.pth", map_location='cpu')
     #print(checkpoint)
     print("#"*30)
-    outputs = single_gpu_test(model, data_loader)
-    print(outputs)
+    #outputs = single_gpu_test(model, data_loader)
+    #print(outputs)
     # for i,d in enumerate(data_loader):
     #     if i % 100 == 0:
     #         print(i)
